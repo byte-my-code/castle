ad62f00ce21930920b9a5c074325790c78ad1e5b
Graham Kerr <graham@bytemycode.co.uk>
2023-10-01 19:17:45 +0100

    Updated build script

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 cxxflags="-I./include -I/usr/include/SDL2 -D_REENTRANT -g -DDEBUG -std=c++17"
 ldflags="-lSDL2 -lpthread"
 
-g++ -c $cxxflags src/renderwindow.cpp
-g++ -c $cxxflags src/game.cpp
-g++ -c $cxxflags src/main.cpp
+if [ -f Castle ]; then
+    rm Castle
+fi
+
+echo Building program...
+
+g++ -c $cxxflags src/renderwindow.cpp -o build/renderwindow.o
+g++ -c $cxxflags src/game.cpp -o build/game.o
+g++ -c $cxxflags src/main.cpp -o build/main.o
 
 g++ *.o -o Castle $ldflags
+
+
+if [ -f Castle ]; then
+    ./Castle
+fi
